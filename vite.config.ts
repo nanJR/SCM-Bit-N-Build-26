c32f604fceac2bb0678bcf726bf690baacc1f300
Rohan N K <93855538+nanJR@users.noreply.github.com>
2026-09-13 10:02:12 +0530

    Semi Final Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     host: '0.0.0.0',
     port: 3000,
